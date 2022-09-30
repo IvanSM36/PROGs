6b3d48cfe7a0cf99c016fe23da9b5ae62f9417cb
IvanSM <ivan_sm@hotmail.es>
2022-09-30 11:00:41 +0200

    Lanzador

---
+++ src/Tema01/Lanzador.java
@@ -0,0 +1,25 @@
+package Tema01;
+
+public class Lanzador {
+	
+	public static void LanzarSumador(Integer n1, Integer n2) {
+		String clase = "Tema01";
+		ProcessBuilder pb;
+		try {
+			pb = new ProcessBuilder("java", clase, n1.toString(), n2.toString());
+		pb.start();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lanzador l = new Lanzador();
+		l.LanzarSumador(1, 51);
+		l.LanzarSumador(51, 100);
+		System.out.println("OK");
+	}
+
+}
