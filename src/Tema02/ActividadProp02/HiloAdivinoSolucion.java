146b4cd4a9dd2d933ea2271d368e927931ab6d2a
IvanSM <ivan_sm@hotmail.es>
2022-11-11 11:57:39 +0100

    SolucionHiloAdivino

---
+++ src/Tema02/ActividadProp02/HiloAdivinoSolucion.java
@@ -0,0 +1,57 @@
+package Tema02.ActividadProp02;
+
+
+import java.util.Random;
+/**
+ * Hilo adivino que intentará encontrar el número oculto
+ * @author Edu López
+ * @version 1.0
+ */
+public class HiloAdivinoSolucion extends Thread{
+	private boolean adivino;
+	private int intentos;
+	private NumeroOcultoSolucion NumeroOcultoSolucion;
+	/**
+	 * Todos los hilos comparten el mismo número oculto y tienen
+	 * un nombre único
+	 * @param NumeroOcultoSolucion Objeto del tipo NumeroOcultoSolucion al que lanzar
+	 * la propuesta de número en cada intento
+	 * @param numeroAdivino El número del adivino (o hilo)
+	 */
+	public HiloAdivinoSolucion(NumeroOcultoSolucion NumeroOcultoSolucion, int numeroAdivino) {
+		super("Adivino" + numeroAdivino);
+		this.NumeroOcultoSolucion = NumeroOcultoSolucion;
+	}
+	@Override
+	public void run() {
+		int resultadoPropuesta=NumeroOcultoSolucion.POR_ADIVINAR;
+		Random rnd = new Random();
+		//para dar igualdad de oportunidades cada hilo
+		//espera un tiempo aleatorio antes de empezar 
+		//los intentos para adivinar el número
+		try {
+			sleep(rnd.nextInt(10));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		while(resultadoPropuesta==NumeroOcultoSolucion.POR_ADIVINAR) {
+			resultadoPropuesta = this.NumeroOcultoSolucion.propuestaNumero(rnd.nextInt(101));
+			this.adivino = (resultadoPropuesta==NumeroOcultoSolucion.LO_HAS_ADIVINADO);
+			this.intentos++;
+		}
+	}
+	/**
+	 * Devuelve true si el hilo ha sido el adivino del número
+	 * @return True si el hilo ha adivinado el número, false en caso contrario
+	 */
+	public boolean esAdivino(){
+		return this.adivino;
+	}
+	/**
+	 * Devuelve el número de intentos empleados en adivinar el número
+	 * @return Número de intentos
+	 */
+	public int getIntentos() {
+		return this.intentos;
+	}
+}
---
+++ src/Tema02/ActividadProp02/NumeroOcultoSolucion.java
@@ -0,0 +1,71 @@
+package Tema02.ActividadProp02;
+
+import java.util.ArrayList;
+import java.util.Random;
+/**
+ * Adivinos de un número oculto
+ * @author Edu López
+ * @version 1.0
+ */
+public class NumeroOcultoSolucion {
+	public static final int LO_HAS_ADIVINADO=1;
+	public static final int YA_ADIVINADO=-1;
+	public static final int POR_ADIVINAR=0;
+	private boolean adivinado = false;
+	private int numero = 0;
+	private ArrayList<HiloAdivinoSolucion> adivinos;
+	public static void main(String[] args) {
+		HiloAdivinoSolucion HiloAdivinoSolucion=null;
+		NumeroOcultoSolucion NumeroOcultoSolucion = new NumeroOcultoSolucion();
+		long tiempoEmpleado=System.currentTimeMillis();
+		Random rnd = new Random();
+		//se genera un número oculto entre 0 y 100, y se guarda
+		NumeroOcultoSolucion.numero = rnd.nextInt(101);
+		//se crea un array con 10 adivinos y se comienza el proceso
+		//de adivinación
+		NumeroOcultoSolucion.adivinos = new ArrayList<HiloAdivinoSolucion>();
+		for(int i=0;i<10;i++) {
+			NumeroOcultoSolucion.adivinos.add(new HiloAdivinoSolucion(NumeroOcultoSolucion,i+1));
+			NumeroOcultoSolucion.adivinos.get(i).start();
+		}
+		//mientras no se adivine el número el hilo principal debe esperar
+		while(!NumeroOcultoSolucion.adivinado);
+		//se obtiene el tiempo empleado en adivinar el número
+		tiempoEmpleado = System.currentTimeMillis()-tiempoEmpleado;
+		//se obtiene el hilo (adivino) que ha adivinado el número
+		/* Lo siguiente usa expresiones lambda, es posible que esto te diese
+		 * problemas de compilación. Lo cambio por las líneas que le siguen
+		 * HiloAdivinoSolucion HiloAdivinoSolucion = (HiloAdivinoSolucion) NumeroOcultoSolucion.adivinos.stream()
+				.filter(adivino -> adivino.esAdivino()).toArray()[0];*/
+		for(HiloAdivinoSolucion adivino:NumeroOcultoSolucion.adivinos) {
+			if(adivino.esAdivino()) {
+				HiloAdivinoSolucion = adivino;
+				break;
+			}
+		}
+		//se muestra información completa sobre el resultado de la 
+		//adivinación
+		System.out.println("El " + HiloAdivinoSolucion.getName() + " ha adivinado el número," +
+		" ha necesitado " + HiloAdivinoSolucion.getIntentos() + " intentos. " +
+		"Se ha tardado " + (float)(tiempoEmpleado/1000F) + " segundos en acertar el número oculto " + 
+				NumeroOcultoSolucion.numero);
+	}
+	/**
+	 * Este método será llamado por cada hilo adivino para realizar cada
+	 * intento de adivinación. El método está sincronizado, quiere decir
+	 * que sólo un hilo podrá ejecutarlo al mismo tiempo, el primero que se
+	 * le conceda la ejecución bloqueará al resto que quiere entrar. Garantizando
+	 * la exclusión mútua.
+	 * @param num Número propuesto
+	 * @return -1 si ya se había adivinado el número, 1 si se adivina el número
+	 *  y 0 si el número todavía no ha sido adivinado
+	 */
+	public synchronized int propuestaNumero(int num) {
+		if(this.adivinado) return YA_ADIVINADO;
+		if(num == this.numero) {
+			this.adivinado = true;
+			return LO_HAS_ADIVINADO;
+		}
+		return POR_ADIVINAR;
+	}
+}
